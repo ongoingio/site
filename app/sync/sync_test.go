aae669a1bf0c4936c918bc93a805ae6b9694b40b
Bernhard Wanger <mail@bernhardwanger.com>
2014-08-28 21:26:09 +0200

    Add sync test

--- app/sync/sync_test.go
+++ app/sync/sync_test.go
@@ -1,9 +1,11 @@
 package sync
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/ongoingio/site/app/database"
+	"gopkg.in/mgo.v2"
+
 	"github.com/ongoingio/site/app/examples"
 	"github.com/ongoingio/site/app/github"
 )
@@ -13,42 +15,102 @@ type githubMock struct {
 }
 
 func (repo *githubMock) Fetch(path string) (*github.Content, []*github.Content, error) {
-	/*
-	   content := &contentMock{
-	       Type:     "file",
-	       Encoding: "base64",
-	       Name:     "README.md",
-	       Path:     "README.md",
-	       SHA:      "7962fa277d1c99417188f9fafe5ac3d575b22133",
-	       URL:      "https://api.github.com/repos/ongoingio/examples/contents/README.md?ref=master",
-	   }
-	   contents := make([]contentMock, 1)
-	   contents[0] = content
-	   repos = &repository.Repository{URL: "foo/bar", Content: contents}
-	*/
+	contentA := &github.Content{
+		Type:     "file",
+		Encoding: "base64",
+		Name:     "File A.md",
+		Path:     "File A.md",
+		SHA:      "123",
+	}
+	contentB := &github.Content{
+		Type:     "file",
+		Encoding: "base64",
+		Name:     "File B.md",
+		Path:     "File B.md",
+		SHA:      "456",
+	}
 
-	return nil, nil, nil
+	if path == "/" {
+		contents := make([]*github.Content, 2)
+		contents[0] = contentA
+		contents[1] = contentB
+		return nil, contents, nil
+	}
+
+	if path == "File A.md" {
+		contentA.Content = "Content A"
+		return contentA, nil, nil
+	}
+
+	if path == "File B.md" {
+		contentB.Content = "Content B"
+		return contentB, nil, nil
+	}
+
+	return nil, nil, fmt.Errorf("path %s not found", path)
+}
+
+type exampleStore struct {
+	data []examples.Example
+}
+
+func (store *exampleStore) Insert(example *examples.Example) error {
+	store.data = append(store.data, *example)
+	return nil
+}
+
+func (store *exampleStore) FindOne(example *examples.Example) error {
+	for _, ex := range store.data {
+		if ex.Path == example.Path {
+			example = &ex
+			return nil
+		}
+	}
+	return mgo.ErrNotFound
+}
+
+func (store *exampleStore) FindAll(examples *[]examples.Example) error {
+	examples = &store.data
+	return nil
+}
+
+func (store *exampleStore) Update(example *examples.Example) error {
+	for i, ex := range store.data {
+		if ex.Path == example.Path {
+			store.data[i] = *example
+		}
+	}
+	return nil
+}
+
+func (store *exampleStore) Remove(example *examples.Example) error {
+	for i, ex := range store.data {
+		if ex.Path == example.Path {
+			// Delete index from slice.
+			store.data = append(store.data[:i], store.data[i+1:]...)
+		}
+	}
+	return nil
 }
 
 func TestSyncNew(t *testing.T) {
-	db := database.Connect("localhost/ongoing-test")
-	defer db.Session.Close()
-	err := db.DropDatabase()
-	if err != nil {
-		panic(err)
-	}
-
-	// Repository
-	repo := &githubMock{
+	github := &githubMock{
 		URL: "/foo",
 	}
 
-	// Model
-	manager := examples.New(db)
+	store := &exampleStore{}
 
-	err = Sync(repo, manager)
+	err := Sync(store, github)
 	if err != nil {
 		t.Fatalf("Sync(): %v", err)
+	}
+
+	if store.data[0].Name != "File A.md" {
+		t.Fatalf("name is %s, should be %s", store.data[0].Name, "File A.md")
+	}
+
+	if store.data[0].Content != "Content A" {
+		t.Fatalf("content is %s, should be %s", store.data[0].Content, "Content A")
 	}
 }
 
